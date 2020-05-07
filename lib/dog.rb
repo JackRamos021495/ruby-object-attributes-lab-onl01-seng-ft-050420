960d8cfca9c6f2f344b2a9b1d46d6be9d09c3204
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 23:57:25 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(breed, name)
+    @breed = breed
+    @name = name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+end
